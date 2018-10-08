2cfa33d2c6bedb78525ecf2cafaf1914a237fd1e
Andy Schuler <andy@solutionx.com>
2018-10-08 17:06:26 -0600

    final answer

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-/** Label with required field display, htmlFor, and block styling and dot optionsssssddddd */
+/** Label with required field display, htmlFor, and block styling and dot !!!!!!!!! */
 function Label({ htmlFor, label, required, requiredDotColor }) {
   return (
     <label style={{ display: "block" }} htmlFor={htmlFor}>
