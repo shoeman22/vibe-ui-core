e38b4a2dcb7c813ea89ddb11af4ed52c72217d8e
Andy Schuler <andy@solutionx.com>
2018-10-08 15:03:42 -0600

    testing deploy

--- src/docs/examples/Label/ExampleOptionalLabel.js
+++ src/docs/examples/Label/ExampleOptionalLabel.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Label from "vibe-ui-core/Label";
 
-/** Optional label */
+/** Optional labelsss */
 export default function ExampleOptionalLabel() {
   return <Label htmlFor="test" label="test" />;
 }
